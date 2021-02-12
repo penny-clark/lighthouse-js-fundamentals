a7ad35b41e1d20e8721e707e01b114c1d46466db
Penny Clark <respect.cactus@gmail.com>
2021-02-12 23:18:00 +0000

    Added more conditional examples with logical operators

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,13 @@
 
-const temperature =-92;
+const temperature = 50;
 
-if (temperature < 0) {
+if (temperature < 0 && temperature > -40) {
   console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) { 
+} else if (temperature < 15 && temperature > 0) { 
   console.log("Short sleeves won't cut it!");
+} else if (temperature < -40 || temperature > 40) {
+  console.log("Actually maybe let's not go outside");
 } else {
 console.log("Short sleeves are fine.");
 }
-console.log("Now you're ready to go outside!");
+
