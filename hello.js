dc9fcf343e0b57335fabd70692e310cebc655be0
Penny Clark <respect.cactus@gmail.com>
2021-02-14 21:44:50 +0000

    Function to say hello and difference between console.log and return

--- hello.js
+++ hello.js
@@ -1,5 +1,20 @@
+//console.log
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+
+}
+sayHelloToConsole('John');
+
+//return
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+/*
+//say hello function
 const sayHello = function(name) {
   console.log("Hello, " + name);
 }
 
-sayHello("Zaphod");
+sayHello("Zaphod");*/
