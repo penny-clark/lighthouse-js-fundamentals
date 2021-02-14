2fad3c99935cbecd27db5f17c6778b0515449c86
Penny Clark <respect.cactus@gmail.com>
2021-02-14 21:42:14 +0000

    Function that produces a result

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+ //option 1 - function returns value to variable 
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+
+//option 2 - call the function directly
+console.log(IsEven(10));
+console.log(IsEven(11));
